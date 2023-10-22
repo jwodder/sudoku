46f39cd1b55f6b68ca4603e22ae547bf56d1de86
John T. Wodder II <git@varonathe.org>
2023-10-22 18:38:44 -0400

    Replace clap with lexopt
    Closes #3.

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,79 @@
 use anyhow::Context;
-use clap::Parser;
+use lexopt::{Arg, Parser};
 use patharg::InputArg;
 use std::process::ExitCode;
 use sudoku::Puzzle;
 
-/// Solve Sudoku puzzles
-#[derive(Parser)]
-#[clap(version)]
-struct Arguments {
-    /// Output the solution with borders and spacing
-    #[clap(short = 'P', long)]
-    pretty: bool,
-
-    /// File containing Sudoku puzzle to solve [default: read from stdin]
-    #[clap(default_value_t, hide_default_value = true)]
-    infile: InputArg,
+#[derive(Clone, Debug, Eq, PartialEq)]
+enum Command {
+    Run { pretty: bool, infile: InputArg },
+    Help,
+    Version,
 }
 
-fn main() -> Result<ExitCode, anyhow::Error> {
-    let args = Arguments::parse();
-    let puzzle = args
-        .infile
-        .read_to_string()
-        .context("Error reading input")?
-        .parse::<Puzzle>()
-        .context("Invalid input")?;
-    match puzzle.solve() {
-        Some(s) => {
-            if args.pretty {
-                println!("{s:#}");
-            } else {
-                println!("{s}");
+impl Command {
+    fn from_parser(mut parser: Parser) -> Result<Command, lexopt::Error> {
+        let mut pretty = false;
+        let mut infile: Option<InputArg> = None;
+        while let Some(arg) = parser.next()? {
+            match arg {
+                Arg::Short('h') | Arg::Long("help") => return Ok(Command::Help),
+                Arg::Short('V') | Arg::Long("version") => return Ok(Command::Version),
+                Arg::Short('P') | Arg::Long("pretty") => pretty = true,
+                Arg::Value(val) if infile.is_none() => {
+                    infile = Some(InputArg::from_arg(val));
+                }
+                _ => return Err(arg.unexpected()),
             }
-            Ok(ExitCode::SUCCESS)
         }
-        None => {
-            eprintln!("No solution");
-            Ok(ExitCode::FAILURE)
+        Ok(Command::Run {
+            pretty,
+            infile: infile.unwrap_or_default(),
+        })
+    }
+
+    fn run(self) -> anyhow::Result<ExitCode> {
+        match self {
+            Command::Run { pretty, infile } => {
+                let puzzle = infile
+                    .read_to_string()
+                    .context("Error reading input")?
+                    .parse::<Puzzle>()
+                    .context("Invalid input")?;
+                match puzzle.solve() {
+                    Some(s) => {
+                        if pretty {
+                            println!("{s:#}");
+                        } else {
+                            println!("{s}");
+                        }
+                        Ok(ExitCode::SUCCESS)
+                    }
+                    None => {
+                        eprintln!("No solution");
+                        Ok(ExitCode::FAILURE)
+                    }
+                }
+            }
+            Command::Help => {
+                println!("Usage: sudoku [-P|--pretty] [<INFILE>]");
+                println!();
+                println!("Solve a Sudoku puzzle");
+                println!();
+                println!("Options:");
+                println!("  -P, --pretty      Output the solution with borders and spacing");
+                println!("  -h, --help        Display this help message and exit");
+                println!("  -V, --version     Show the program version and exit");
+                Ok(ExitCode::SUCCESS)
+            }
+            Command::Version => {
+                println!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+                Ok(ExitCode::SUCCESS)
+            }
         }
     }
+}
+
+fn main() -> anyhow::Result<ExitCode> {
+    Command::from_parser(Parser::from_env())?.run()
 }
